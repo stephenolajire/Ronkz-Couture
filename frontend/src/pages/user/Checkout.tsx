77c48e6653d8ac69a7553776b44995cddb414f15
olajirestephen <horlharmighty2000@gmail.com>
2025-08-14 04:46:18 -0600

    added cart and checkout page

--- frontend/src/pages/user/Checkout.tsx
+++ frontend/src/pages/user/Checkout.tsx
@@ -9,6 +9,7 @@ import {
   ShoppingBag,
 } from "lucide-react";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const orderItems = [
   {
@@ -900,13 +901,15 @@ const CheckoutPage:React.FC = () => {
                       Place Order
                     </button>
                   ) : (
-                    <button
-                      type="button"
-                      onClick={() => window.location.reload()}
-                      className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
-                    >
-                      Continue Shopping
-                    </button>
+                    <Link to="/shop">
+                      <button
+                        type="button"
+                        onClick={() => window.location.reload()}
+                        className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+                      >
+                        Continue Shopping
+                      </button>
+                    </Link>
                   )}
                 </div>
               </form>
